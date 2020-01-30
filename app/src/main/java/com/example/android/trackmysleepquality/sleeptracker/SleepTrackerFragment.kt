8876ba5485c3cb0f81b0f3c9aa8a38aaefb7a4dd
Ahmer Mughal <a2mughal143@gmail.com>
2020-01-30 18:17:27 +0500

    Added Linear RecyclerView used data binding for recycler view list adapter using DiffUtils binding adapters

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -59,6 +59,13 @@ class SleepTrackerFragment : Fragment() {
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            adapter.submitList(it)
+        })
+
         binding.setLifecycleOwner(this)
 
         sleepTrackerViewModel.navigateToSleepQuality.observe(this, Observer { night->
